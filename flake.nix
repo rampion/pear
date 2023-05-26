9a35e8b719e6afe235e994813f22a43e94ee63ec
Noah Luck Easterly <noah.easterly@gmail.com>
2023-05-25 21:24:54 -0400

    try out flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "The pear package for haskell, describing and defining the PearTree";
+  inputs.nixpkgs.url = "nixpkgs";
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      });
+    in
+    {
+      overlay = (final: prev: {
+        pear = final.haskellPackages.callCabal2nix "pear" ./. {};
+      });
+      packages = forAllSystems (system: {
+         pear = nixpkgsFor.${system}.pear;
+      });
+      defaultPackage = forAllSystems (system: self.packages.${system}.pear);
+      checks = self.packages;
+      devShell = forAllSystems (system: let haskellPackages = nixpkgsFor.${system}.haskellPackages;
+        in haskellPackages.shellFor {
+          packages = p: [self.packages.${system}.pear];
+          withHoogle = true;
+          buildInputs = with haskellPackages; [
+            haskell-language-server
+            ghcid
+            cabal-install
+            hoogle
+          ];
+          # Change the prompt to show that you are in a devShell
+          shellHook = "export PS1='\\e[1;34mdev > \\e[0m'";
+
+          name="pear";
+        });
+  };
+}
